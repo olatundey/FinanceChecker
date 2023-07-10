3eb8e6e2447e81ce5182cb4bf63563b0cf2b4c8a
Olatunde Yusuf <123118889+olatundey@users.noreply.github.com>
2023-07-10 11:59:43 +0100

    data

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceChecker.Data
+{
+	public class ApplicationDbContext :DbContext
+	{
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //          modelBuilder.Entity<nameofModelCLASS>().HasData(
+            //);
+
+        }
+    }
+}
+
